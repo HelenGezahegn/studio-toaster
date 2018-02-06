c8211f55ea515fc617908c5741af57f002a39c3d
Bruce Nie <brucenie@Bruces-MacBook-Pro.local>
2018-02-05 18:11:31 -0700

    tutorial level finished

--- Bit-by-Bit_1/js/plugins.js
+++ Bit-by-Bit_1/js/plugins.js
@@ -3,6 +3,8 @@
 var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
-{"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"false","Made with MV Image":"MadeWithMv","Show Custom Splash":"true","Custom Image":"STUDIOtoaster","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
-{"name":"no_title_screen","status":true,"description":"","parameters":{}}
+{"name":"GALV_LayerGraphics","status":false,"description":"(v.2.0) Create graphic layers for parallax mapping, fog, etc. View the 'Help' document for plugin commands and info.","parameters":{"Tile Size":"48"}},
+{"name":"MadeWithMv","status":false,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"false","Made with MV Image":"MadeWithMv","Show Custom Splash":"true","Custom Image":"STUDIOtoaster","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
+{"name":"shadow","status":true,"description":"Version 1.053 Adds Simple Shadows to characters\n<QuasiSimpleShadows>","parameters":{"Default Source Radius":"240","=====================":"","Enable Shadow Blur":"false","Shadow Blur Strength":"7","Enable Shadow Zoom":"true"}},
+{"name":"TerraxLighting","status":true,"description":"v1.5.1 Creates an extra layer that darkens a map and adds lightsources!","parameters":{"Player radius":"30","Add to options":"Yes","Option menu entry":"Lighting effects","Reset Lights":"No","Save DaynightHours":"0","Save DaynightMinutes":"0","Save DaynightSeconds":"0","Flashlight offset":"0","Screensize X":"866","Screensize Y":"630","Kill Switch":"No"}}
 ];
